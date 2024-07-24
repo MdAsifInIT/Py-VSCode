af0e6239ed1a41fbce88caf7f798a9d2806577bd
MdAsifLM <87461866+MdAsifLM@users.noreply.github.com>
2024-07-24 17:53:09 +0530

    Removing unnecessary code

--- abhinavjhatu.py
+++ abhinavjhatu.py
@@ -1,14 +1,4 @@
-# name = "Abhinav"
-# attitude = "Good"
-# face = "Happy"
-# skills = "Python"
-# harkat = "Lodu"
 
-# print(name, "is a", harkat)
-
-# # print(name[1])
-# # for character in name:
-# #   print(character)
 nm = "Harry"
 print(nm[-4:-2])         #1-
 print(len(nm))
